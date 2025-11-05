c4e869f9d8f948ce2faa64b6a67677799e76dce9
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-05 12:28:34 +0600

    modification

--- database/migrations/2025_10_22_092414_create_authors_table.php
+++ database/migrations/2025_10_22_092414_create_authors_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
      
     public function up(): void
     {
+        
         Schema::create('authors', function (Blueprint $table) {
             
             $table->id();
