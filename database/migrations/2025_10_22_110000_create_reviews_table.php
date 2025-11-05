c37701fa0de6415ca3b746ab7a464e9779108f6f
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-05 12:28:17 +0600

    modification

--- database/migrations/2025_10_22_110000_create_reviews_table.php
+++ database/migrations/2025_10_22_110000_create_reviews_table.php
@@ -12,6 +12,7 @@ return new class extends Migration
     
     public function up(): void
     {
+        
         Schema::create('reviews', function (Blueprint $table) {
             $table->id();
             $table->foreignId('book_id')->constrained();
