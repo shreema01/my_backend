9ab6252d638c8e9cabf5dfe0157fc66cfe2cf818
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-06 17:00:39 +0600

    modification

--- routes/api.php
+++ routes/api.php
@@ -8,13 +8,10 @@ use App\Http\Controllers\API\AuthenticationController;
 use App\Http\Controllers\AboutController;
 use PharIo\Manifest\AuthorCollection;
 
-// Public routes
 Route::post('register', [AuthenticationController::class, 'register']);
 Route::post('login', [AuthenticationController::class, 'login']);
 Route::get('about', [AuthenticationController::class, 'index']);
 
-
-// Protected routes
 Route::middleware('auth:sanctum')->group(function () {
     Route::get('get-user', [AuthenticationController::class, 'userInfo']);
     Route::post('logout', [AuthenticationController::class, 'logOut']);
