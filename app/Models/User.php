070235ca5726051826cecfa540c0a466a930ec78
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-08 20:14:29 +0600

    modification

--- app/Models/User.php
+++ app/Models/User.php
@@ -10,7 +10,6 @@ use Laravel\Sanctum\HasApiTokens;
 
 class User extends Authenticatable
 {
-    
     use HasApiTokens,HasFactory, Notifiable;
     /** @use HasFactory<\Database\Factories\UserFactory> */
     /**
