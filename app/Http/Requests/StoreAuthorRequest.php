2306376a9a8dbf676985fa87ed10d7908f87e288
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-12 10:23:22 +0600

    modification update

--- app/Http/Requests/StoreAuthorRequest.php
+++ app/Http/Requests/StoreAuthorRequest.php
@@ -14,6 +14,7 @@ class StoreAuthorRequest extends FormRequest
 
     public function rules(): array
     {
+        
         return [
             'title'                 => 'required|string|max:255',
             'description'           => 'required|string|max:255',
