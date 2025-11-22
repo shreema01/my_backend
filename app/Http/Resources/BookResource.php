956dff323b6404849d177f46d7a1400c60dfeea6
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-22 21:41:23 +0600

    modofication

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -19,22 +19,15 @@ class BookResource extends JsonResource
             'price'          => $this->price,
             'rating'         => $this->rating,
 
-            // Casted array fields (auto array)
             'genre'          => $this->genre ?? [],
             'readers_love'   => $this->readers_love ?? [],
 
             'sample_chapter' => $this->sample_chapter,
 
-            // Full image URL
-            // 'cover_image' => $this->cover_image
-            //     ? url('/uploads/books/' . $this->cover_image)
-            //     : null,
-
                  'cover_image' => $this->cover_image
     ? env('NEXT_PUBLIC_LARAVEL_HOST') . '/uploads/books/' . $this->cover_image
     : null,
 
-            // Standard timestamps
             'created_at'     => $this->created_at?->format('Y-m-d H:i:s'),
             'updated_at'     => $this->updated_at?->format('Y-m-d H:i:s'),
         ];
