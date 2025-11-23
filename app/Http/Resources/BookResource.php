81eafc98170c969ce6781ee3db953aa6dab607d7
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-11-23 17:01:18 +0600

    modification

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -4,6 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
+
 class BookResource extends JsonResource
 {
     /**
@@ -12,23 +13,23 @@ class BookResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
-            'id'             => $this->id,
-            'title'          => $this->title,
-            'description'    => $this->description,
-            'price'          => $this->price,
-            'rating'         => $this->rating,
+            'id' => $this->id,
+            'title' => $this->title,
+            'description' => $this->description,
+            'price' => $this->price,
+            'rating' => $this->rating,
 
-            'genre'          => $this->genre ?? [],
-            'readers_love'   => $this->readers_love ?? [],
+            'genre' => $this->genre ?? [],
+            'readers_love' => $this->readers_love ?? [],
 
             'sample_chapter' => $this->sample_chapter,
 
-                 'cover_image' => $this->cover_image
-    ? env('NEXT_PUBLIC_LARAVEL_HOST') . '/uploads/books/' . $this->cover_image
+            'cover_image' => $this->cover_image
+    ? env('NEXT_PUBLIC_LARAVEL_HOST').'/uploads/books/'.$this->cover_image
     : null,
 
-            'created_at'     => $this->created_at?->format('Y-m-d H:i:s'),
-            'updated_at'     => $this->updated_at?->format('Y-m-d H:i:s'),
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
         ];
     }
 }
