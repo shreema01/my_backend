070304e94182a04f25a46936b34a9784fd3871e7
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-12-03 16:47:10 +0600

    modification

--- app/Http/Resources/AuthorResource.php
+++ app/Http/Resources/AuthorResource.php
@@ -12,6 +12,7 @@ class AuthorResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
+       
         return [
             'id' => $this->id,
             'title' => $this->title,
