17a094b7e432016a9667c58ee6465f3e65427507
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-12-03 16:49:37 +0600

    modification

--- app/Models/Author.php
+++ app/Models/Author.php
@@ -6,8 +6,8 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 class Author extends Model
 {
+    
     use HasFactory;
-
     protected $table = 'authors';
     protected $fillable = [
         'title',
