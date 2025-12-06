e576d918d7a470ecba9a215a3a4f0ee21ac98710
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-12-06 16:12:09 +0600

    modification

--- app/Http/Resources/BookResource.php
+++ app/Http/Resources/BookResource.php
@@ -14,6 +14,7 @@ class BookResource extends JsonResource
     {
         
         return [
+            
             'id' => $this->id,
             'title' => $this->title,
             'description' => $this->description,
