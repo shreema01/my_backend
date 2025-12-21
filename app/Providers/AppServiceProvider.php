47bb055ce1d2d560fe6c7af4a85d1dac032414dd
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2025-12-21 16:48:35 +0600

    modification

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -20,7 +20,8 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(AuthorRepositoryInterface::class, AuthorRepository::class);
          
         // Book
-         $this->app->bind(BookRepositoryInterface::class, BookRepository::class);    
+         
+        $this->app->bind(BookRepositoryInterface::class, BookRepository::class);    
 
     }
     /**
