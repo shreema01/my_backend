ceea2e82213607607c1969d91ad335ab9f720a7e
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-01-20 10:56:50 +0600

    modification

--- app/Http/Requests/StoreBookRequest.php
+++ app/Http/Requests/StoreBookRequest.php
@@ -16,6 +16,7 @@ class StoreBookRequest extends FormRequest
     public function rules(): array
     {
         return [
+           
             'title' => 'required|string|max:255',
             'description' => 'required|string|max:255',
             'price' => 'required|string|max:255',
