18dfd84a1ea34c8872f1136bdbc6b26486a8b4d2
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-01-20 10:57:25 +0600

    modification

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -40,6 +40,7 @@ class UserFactory extends Factory
      */
     public function unverified(): static
     {
+        
         return $this->state(fn (array $attributes) => [
             'email_verified_at' => null,
         ]);
