2cc08409a3acd2db5cf74a021de0bcabf5395713
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-01-21 09:31:52 +0600

    modification

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -44,6 +44,7 @@ class UserFactory extends Factory
         return $this->state(fn (array $attributes) => [
             
             'email_verified_at' => null,
+            
         ]);
     }
 }
