e9a802866042f971cc0f18e46e8bc35395e801ef
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-01-25 13:07:51 +0600

    modification

--- app/Services/BookService.php
+++ app/Services/BookService.php
@@ -6,7 +6,6 @@ use App\Repositories\BookRepository;
 class BookService
 {
     protected $bookRepository;
-    
     public function __construct(BookRepository $bookRepository)
     {
         $this->bookRepository = $bookRepository;
