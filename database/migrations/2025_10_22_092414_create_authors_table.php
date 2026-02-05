08241cf5fd5ca9444a38e53c64c42f53a6dd860e
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-02-05 17:27:00 +0600

    modification

--- database/migrations/2025_10_22_092414_create_authors_table.php
+++ database/migrations/2025_10_22_092414_create_authors_table.php
@@ -15,8 +15,9 @@ return new class extends Migration
         
         Schema::create('authors', function (Blueprint $table) {
             
-            
+    
             $table->id();
+            
             $table->string('title');
             $table->string('description');
             $table->text('story');
