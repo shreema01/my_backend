65e347ede61aa88d2b962eb1803ba41b71241b30
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-03-16 15:12:16 +0600

    modification

--- app/Services/AuthorService.php
+++ app/Services/AuthorService.php
@@ -20,8 +20,7 @@ class AuthorService
     public function getAuthorDetails($id)
     {
         
-        
-       return $this->authorRepository->getAuthorById($id);
+        return $this->authorRepository->getAuthorById($id);
     }
     
     public function addAuthor(array $data)
