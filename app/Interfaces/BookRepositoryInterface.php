74b33e18943b05afaa405a5d57b828d5563660fe
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-03-30 14:47:23 +0600

    modification

--- app/Interfaces/BookRepositoryInterface.php
+++ app/Interfaces/BookRepositoryInterface.php
@@ -8,5 +8,6 @@ interface BookRepositoryInterface
     public function getBookById($id);
     public function createBook(array $data);
     public function updateBook($id, array $data);
+    
     public function deleteBook($id);
 }
