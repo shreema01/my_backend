f2aa66440fe4abf2eceaa69645fb2255d1a2a428
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-03-30 14:50:09 +0600

    modification

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,6 +21,7 @@ class RouteServiceProvider extends ServiceProvider
 
             Route::middleware('web')
                 ->group(base_path('routes/web.php'));
+                
         });
     }
 }
