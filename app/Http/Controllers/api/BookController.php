4790073f888e20be684cd146a8ee113a9cce8cd8
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-04-02 22:37:33 +0600

    modification

--- app/Http/Controllers/api/BookController.php
+++ app/Http/Controllers/api/BookController.php
@@ -14,6 +14,7 @@ class BookController extends Controller
     //     $this->bookService = $bookService;
     // }
     public function __construct(private readonly BookService $bookService) {}
+    
     public function index()
     {
         // $books = $this->bookService->getAllBooks();
