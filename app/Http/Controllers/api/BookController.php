247fd5697b5c54aba32a249338eccec04b523cd4
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-04-07 21:58:18 +0600

    modification

--- app/Http/Controllers/api/BookController.php
+++ app/Http/Controllers/api/BookController.php
@@ -37,7 +37,8 @@ class BookController extends Controller
 
     public function store(StoreBookRequest $request)
     {
-       $book = $this->bookService->addBook($request->validated());
+       
+        $book = $this->bookService->addBook($request->validated());
 
         // $validatedData = $request->validated();
 
