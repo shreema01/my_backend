05d98aed7e3d2f1f775e0b8eea3ec5a195d19d23
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-05-24 09:51:34 +0600

    modification

--- app/Http/Controllers/api/AuthorController.php
+++ app/Http/Controllers/api/AuthorController.php
@@ -13,9 +13,7 @@ class AuthorController extends Controller
 
     public function __construct(AuthorService $authorService)
     {
-        
        $this->authorService = $authorService;
-       
     }
 
     public function index()
