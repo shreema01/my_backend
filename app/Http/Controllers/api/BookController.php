770f90ff007939707b749cf19aac9e3a61271fee
Shreema Dey Purba <122149624+shreema2347@users.noreply.github.com>
2026-08-22 18:37:27 +0600

    modification

--- app/Http/Controllers/api/BookController.php
+++ app/Http/Controllers/api/BookController.php
@@ -63,6 +63,7 @@ class BookController extends Controller
             
             'message' => 'Book updated successfully',
             'book' => $book,
+            
         ]);
         
     }
